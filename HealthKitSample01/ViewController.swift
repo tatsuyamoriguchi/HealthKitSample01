b45085cd80a790c2c688255ca03f6f1d4c35b5bb
Brian Moriguchi <brian@beckos.com>
2020-06-28 15:58:36 -0700

    Initial Commit

---
+++ HealthKitSample01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthKitSample01
+//
+//  Created by Tatsuya Moriguchi on 6/28/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
